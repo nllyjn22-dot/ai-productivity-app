ad74fc1b65d9206c9a7a62721c8706c7a6289e62
NILAY JANI <nilay@Mac-1.lan>
2026-03-26 09:40:58 +0530

    fix: vercel routing clean url

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Tasks from "./pages/Tasks";
 import Notes from "./pages/Notes";
@@ -6,7 +6,9 @@ import Focus from "./pages/Focus";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
+      {/* <h1>AI Productivity App</h1> */}
+
       <nav className="navbar">
         <div>AI App</div>
         <div>
@@ -23,9 +25,7 @@ function App() {
         <Route path="/notes" element={<Notes />} />
         <Route path="/focus" element={<Focus />} />
       </Routes>
-      {/* </BrowserRouter> */}
-    </HashRouter>
-    // {/* <h1>AI Productivity App</h1> */}
+    </BrowserRouter>
   );
 }
 
